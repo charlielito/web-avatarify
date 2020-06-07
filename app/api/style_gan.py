5a34415a831bc1ede0ac7b836b830e0051002883
charlielito <candres.alv@gmail.com>
2020-06-06 20:45:01 -0500

    save

--- app/api/style_gan.py
+++ app/api/style_gan.py
@@ -28,7 +28,9 @@ def get_avatar(credentials: HTTPAuthorizationCredentials = security.http_credent
     r = requests.get(url, headers={"User-Agent": "My User Agent 1.0"}).content
 
     image = io.bytes2image(r)
-    print(image.shape)
+    image = cv2.resize(image, (512, 512))
+    r = io.image2bytes(image)
+    # print(image.shape)
 
     base64image = base64.b64encode(r)
     return Response(avatar=types.Image(content=base64image))
--- web-app/src/axios-avatarify.js
+++ web-app/src/axios-avatarify.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const tokenAuth = 'TxcbV6X7mMw7agE8mgvO3GxymZGUCLJa';
 // const url = 'http://localhost:8008';
 // const url = 'http://18.219.77.246';
-const url = 'http://api.liveportraits.ml';
+// const url = 'http://api.liveportraits.ml';
+const url = 'http://0.0.0.0:8008';
 const timeoutMinutes = 1;
 
 const instance = axios.create({
