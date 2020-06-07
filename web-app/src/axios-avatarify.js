42014df2cb30dd4d08c9a3fb7447d74971800a39
charlielito <candres.alv@gmail.com>
2020-06-06 20:46:37 -0500

    fix

--- web-app/src/axios-avatarify.js
+++ web-app/src/axios-avatarify.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const tokenAuth = 'TxcbV6X7mMw7agE8mgvO3GxymZGUCLJa';
-// const url = 'http://localhost:8008';
+const url = 'http://localhost:8008';
 // const url = 'http://18.219.77.246';
-const url = 'http://api.liveportraits.ml';
+// const url = 'http://api.liveportraits.ml';
 const timeoutMinutes = 1;
 
 const instance = axios.create({
