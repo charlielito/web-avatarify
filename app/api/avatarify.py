b4062c100ce7e62c2873309c1ce24b880d020777
charlielito <candres.alv@gmail.com>
2020-06-06 20:38:42 -0500

    save

--- app/api/avatarify.py
+++ app/api/avatarify.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 import cv2
 import numpy as np
+import requests
 from fastapi import APIRouter
 from fastapi.exceptions import HTTPException
 from fastapi.security import HTTPAuthorizationCredentials
@@ -15,8 +16,8 @@ from pydantic import BaseModel
 
 from app import io, model_funs, security, types
 
-
 router = APIRouter()
+server_url = os.getenv("SERVER_URL")
 
 
 class Request(BaseModel):
@@ -30,77 +31,13 @@ class Response(BaseModel):
     video: types.Video
 
 
-def handle_image_request(image: types.Image):
-    if image.content is None and image.source is None:
-        raise HTTPException(
-            status_code=400,
-            detail="Either image as bytes or image source with uri needs to be sent!",
-        )
-
-    try:
-        init = time.time()
-        if image.content is not None:
-            image_bytes = image.content
-            image = io.bytes2image(base64.b64decode(image_bytes))
-        else:
-            image_bytes = io.read_fn(image.source.imageUri)
-            image = io.bytes2image(image_bytes)
-            image_bytes = base64.b64encode(image_bytes)
-        print(f"Elapsed reading image: {time.time()-init}")
-    except Exception as e:
-        raise HTTPException(
-            status_code=500,
-            detail="Image {} could not be read. Error: {}".format(
-                image.source.imageUri, e
-            ),
-        )
-
-    return image, image_bytes
-
-
 @router.post("")
 def run_inference(
     request: Request,
     credentials: HTTPAuthorizationCredentials = security.http_credentials,
 ):
-    avatar, _ = handle_image_request(request.avatar)
-    avatar = cv2.resize(avatar, model_input_size)
-    if avatar.ndim == 2:
-        avatar = np.tile(avatar[..., None], [1, 1, 3])
-    print(avatar.shape)
-    # cv2.imshow("original", avatar)
-    # cv2.waitKey(1001)
-    model.set_source_image(avatar)
+    response = requests.post(server_url, data=request)
+    if response.status_code != 200:
+        raise HTTPException(response.status_code, response.text)
 
-    video_bytes = base64.b64decode(request.video.content)
-    video_frames = list(io.bytes2video(video_bytes))
-    print(len(video_frames))
-
-    # audio = io.get_audio_obj(video_bytes)
-
-    # output_frames = model_funs.generate_video(
-    #     model,
-    #     video_frames,
-    #     merge=request.merge,
-    #     axis=request.axis,
-    #     verbose=True,
-    #     model_input_size=model_input_size,
-    # )
-
-    # fps = 30.0
-    # video = VideoClip(
-    #     lambda t: output_frames[int(t * fps)], duration=len(video_frames) / fps
-    # )
-    # video = video.set_audio(audio.set_duration(video.duration))
-
-    path = f"app/static/{uuid.uuid4().hex}.mp4"
-
-    # video.write_videofile(path, fps=fps)
-
-    output_frames = video_frames
-    io.write_video(path, output_frames)
-
-    video_bytes = io.read_fn(path)
-    result = base64.b64encode(video_bytes).decode()
-
-    return Response(video=types.Video(content=result))
+    return response.json()
